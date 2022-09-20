d45ecc028f8f84f938889982a47d78d865c0aa8e
RAHUL KUMAR <techierahul404@gmail.com>
2022-09-20 13:15:15 +0530

    Delete .history directory

--- .history/src/state/store_20220609235734.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import counterReducer from './countReducer';
-
-export const store = configureStore({
-    reducer: {
-        counter: counterReducer
-    },
-})
